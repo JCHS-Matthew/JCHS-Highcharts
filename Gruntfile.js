9696190babd7b677d1c8c60b916ee267050dd7bc
Matthew Arck <matthew_arck@harvard.edu>
2018-07-17 15:22:28 -0400

    add PostCSS and JSDoc tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,52 +1,114 @@
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  require("load-grunt-tasks")(grunt);
+  require("load-grunt-tasks")(grunt) //loads all Grunt NpmTasks automatically
 
-    // Project configuration.
-    grunt.initConfig({
-      pkg: grunt.file.readJSON('package.json'),
-      babel: {
-        options: {
-          presets: ['env'] //compiles to JS compatible with most browsers (using browserslist default setting)
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    /* ESLint */
+    // lint our JS
+    eslint: {
+      options: {
+        "extends": "eslint:recommended",
+        "rules": {
+          // enable additional rules
+          "indent": ["warn", 2],
+          "semi": ["warn", "never"],
+          "valid-jsdoc": "warn",
+          "no-confusing-arrow": "warn",
+          "no-magic-numbers": "off",
         },
-        dist: {
-          files: {
-            "dist/JCHS-highcharts.js": "js/JCHS-highcharts.js"
-          }
-        }
+        "maxWarnings": 20,
       },
-      uglify: {
-        build: {
-          src: 'dist/JCHS-highcharts.js',
-          dest: 'dist/JCHS-highcharts.min.js'
-        }
-      },
-      jshint: {
-        // define the files to lint
-        files: ['Gruntfile.js', 'js/*.js', 'test/**/*.js'],
-        options: {
-          asi: true, //supress warnings about missing semicolons
-          esversion: 6, //allow ES6 syntax (will be compiled down to ~ES5 by Babel)
-          globals: {
-            jQuery: true,
-          }
-        }
-      },
+      target: ['Gruntfile.js', 'src/js/*.js', 'test/**/*.js']
+    },
 
-      //unit testing
-      karma: {
-        unit: {
-          configFile: 'karma.conf.js'
+    /* Babel */
+    // transpile our JS (which uses new ES6 syntax) down to older, more browser compatible JS syntax
+    babel: {
+      options: {
+        presets: ['env'] //compiles to JS compatible with most browsers (using browserslist default setting)
+      },
+      dist: {
+        files: {
+          'build/JCHS-highcharts.transpiled.js': 'src/js/JCHS-highcharts.js'
         }
       }
-    });
-  
+    },
 
+    /* Browserify */
+    // bundle all JS into one file (JHCS, Highcharts, and jQuery)
+    browserify: {
+      dist: {
+        src: 'build/JCHS-highcharts.transpiled.js',
+        dest: 'dist/JCHS-highcharts.js'
+      }
+    },
 
-    // Default task(s).
-    grunt.registerTask('default', ['jshint', 'babel', 'uglify']);
-    grunt.registerTask('test', ['karma']);
+    /* PostCSS */
+    // Stylelint, PostCS Preset Env, Autoprefixer, CSSNano 
+    // lint, transpile, add vendor prefixes, and minify CSS
+    postcss: {
+      options: {
+        map: false,
+        processors: [
+          require("stylelint")({ //linting 
+            "rules": {
+              "block-no-empty": true,
+              "color-no-invalid-hex": true,
+              "comment-empty-line-before": [ "always", { "ignore": ["stylelint-commands", "after-comment"] } ],
+              "declaration-colon-space-after": "always",
+              "indentation": [2],
+              "max-empty-lines": 2,
+              "rule-empty-line-before": [ "always", { "except": ["first-nested"], "ignore": ["after-comment"] } ]
+            }
+          }),
+          //require('postcss-import'),
+          require('postcss-preset-env'), //polyfill any new CSS features
+          require('autoprefixer'), //add vendor prefixes
+          require('cssnano')() // minify the result
+        ],
+      },
+      dist: {
+        src: 'src/css/*.css',
+        dest: 'dist/JCHS-highcharts.min.css'
+      }
+    },
 
+    /* Karma */
+    //cross-browser unit testing
+    karma: {
+      unit: {
+        configFile: 'karma.conf.js'
+      }
+    },
 
-  };
+    /* Uglify */
+    //minify the final JS file
+    uglify: {
+      build: {
+        src: 'dist/JCHS-highcharts.js',
+        dest: 'dist/JCHS-highcharts.min.js'
+      }
+    },
+
+    jsdoc: {
+      dist: {
+        src: ['src/js/*.js', 'README.md'],
+        options: {
+          destination: 'docs',
+          template: "node_modules/minami"
+        }
+      }
+    }
+  })
+
+  // name the tasks 
+  grunt.registerTask('default', ['eslint', 'jsdoc', 'babel', 'postcss', 'browserify', 'karma', 'uglify'])
+  grunt.registerTask('test', ['karma'])
+  grunt.registerTask('css', ['postcss'])
+  grunt.registerTask('js', ['eslint', 'jsdoc', 'babel', 'browserify', 'uglify'])
+
+}
