e5ce9fd31b95e5e8a90d78ebf21bf0a4d5ba506d
Matthew Arck <matthew_arck@harvard.edu>
2018-12-20 15:14:29 -0500

    Add H object references in unit test

--- test/units.js
+++ test/units.js
@@ -1,19 +1,20 @@
-/* global JCHS */ //ESLint ignore
+
+var H = Highcharts 
 
 QUnit.test('numFormat() test', function (assert) {
-  assert.equal(JCHS.numFormat(1234.5678, 2), '1,234.57')
-  assert.equal(JCHS.numFormat(1234.321, 2), '1,234.32')
-  assert.equal(JCHS.numFormat(1234.5678, 0), '1,235')
-  assert.equal(JCHS.numFormat(1234.5), '1,234.5')
-  assert.equal(JCHS.numFormat(1234567, 0), '1,234,567')
-  assert.equal(JCHS.numFormat(1234567), '1,234,567')
-  assert.equal(JCHS.numFormat(7654.321), '7,654.32')
+  assert.equal(H.JCHS.numFormat(1234.5678, 2), '1,234.57')
+  assert.equal(H.JCHS.numFormat(1234.321, 2), '1,234.32')
+  assert.equal(H.JCHS.numFormat(1234.5678, 0), '1,235')
+  assert.equal(H.JCHS.numFormat(1234.5), '1,234.5')
+  assert.equal(H.JCHS.numFormat(1234567, 0), '1,234,567')
+  assert.equal(H.JCHS.numFormat(1234567), '1,234,567')
+  assert.equal(H.JCHS.numFormat(7654.321), '7,654.321')
 })
 
 QUnit.test("requestURL() test", function (assert) {
   var param1 = '1-2qNM31nv35RaGligm1c9RG2TN94NIYJ3fMDBUt8Sso',
     param2 = 'Sheet2!A:X'
-  assert.equal(JCHS.requestURL(param1, param2), "https://sheets.googleapis.com/v4/spreadsheets/1-2qNM31nv35RaGligm1c9RG2TN94NIYJ3fMDBUt8Sso/values/Sheet2!A:X?key=AIzaSyDY_gHLV0A7liVYq64RxH7f7IYUKF15sOQ&valueRenderOption=UNFORMATTED_VALUE")
+  assert.equal(H.JCHS.requestURL(param1, param2), "https://sheets.googleapis.com/v4/spreadsheets/1-2qNM31nv35RaGligm1c9RG2TN94NIYJ3fMDBUt8Sso/values/Sheet2!A:X?key=AIzaSyDY_gHLV0A7liVYq64RxH7f7IYUKF15sOQ&valueRenderOption=UNFORMATTED_VALUE")
 })
 
 
@@ -27,3 +28,7 @@ QUnit.test("get request results test (Google Sheets API)", function (assert) {
   })
 })
 
+QUnit.test("placeholder test", function (assert) {
+  //JCHS.yAxisTitle({foo: bar}, 'foo', 'bar')
+  assert.equal(1, 1)
+})
